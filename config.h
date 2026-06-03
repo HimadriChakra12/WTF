7351bebf56a4c76cf88c19255293238f8b0ba74f
Himadri Chakrabarty <107918769+HimadriChakra12@users.noreply.github.com>
2026-06-03 06:29:36 +0000

    Update config.h

--- config.h
+++ config.h
@@ -1,13 +1,5 @@
 #include "termbox2.h"
-
-/*
- * TUI direction.
- *
- * DIRECTION_BTM - From bottom to top.
- * DIRECTION_TOP - From top to bottom.
- */
-#define DIRECTION_BTM
-// #define DIRECTION_TOP
+#define DIRECTION_TOP
 
 /*
  * Indicates the highest possible entry inaccuracy from the query,
@@ -16,24 +8,20 @@
  * Recommended value: 1
  */
 #define FUZZ_MAX_INACCURACY 1
-
-/*
- * Character used to fill the status bar. (Can be unicode)
- */
-#define STATUS_BAR_FILL "-"
+#define STATUS_BAR_FILL " "
 /* You can use the Em Dash to fill in the gaps. */
 // #define STATUS_BAR_FILL "—"
-#define STATUS_BAR_FILL_SZ 1
+#define STATUS_BAR_FILL_SZ 2
 
 /*
  * Color of the status bar.
  * Colors are defined in `termbox2.h:253`.
  */
-#define STATUS_BAR_COLOR TB_YELLOW
+#define STATUS_BAR_COLOR TB_WHITE
 
 #define SELECTOR "|"
 #define SELECTOR_SZ 1
-#define SELECTOR_COLOR TB_GREEN | TB_BOLD
+#define SELECTOR_COLOR TB_WHITE | TB_BOLD
 
 #define QUERY_PREFIX ">"
 #define QUERY_PREFIX_SZ 1
